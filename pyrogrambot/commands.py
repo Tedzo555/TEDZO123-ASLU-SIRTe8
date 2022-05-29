24b5f7e09496fee0d4d1eb6a00a63e8009b147eb
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2022-05-29 15:35:47 +0530

    Update commands.py

--- pyrogrambot/commands.py
+++ pyrogrambot/commands.py
@@ -53,12 +53,3 @@ Usᴇʀɴᴀᴍᴇ : @{msg.chat.username}
 Cʜᴀᴛ ɪᴅ : `{msg.chat.id}`
 Usᴇʀ ɪᴅ : `{msg.from_user.id}`"""
     await msg.reply_text(text=text)
-@Client.on_message(filters.private & filters.command("tedzo"))
-async def start_message(bot, message):             
-    await message.reply_photo(
-        photo=random.choice(PHOTOS),
-        caption=f"Hello {message.from_user.mention}👋🏻\nI'am A Multi use Bot with many usefull features.\neg:- Telegarph, Channel ID, User ID, Fun, Group Id etc...\nYou can see My commands by below button... \n\n◉ send channel last message with forwerd tag to get the channel id 💯",              
-        reply_markup=InlineKeyboardMarkup(TEDZO_BUTTO)
-    )
-
-            
