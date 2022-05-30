090602cd3e8976d8bfc6c0719ba7a102010d7530
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2022-05-30 16:39:38 +0530

    Create tgraph.py

---
+++ pyrogrambot/tgraph.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+@Webot.on_message(filters.photo)
+async def telegraphphoto(client, message):
+    msg = await message.reply_text("Uploading To Telegraph...")
+    download_location = await client.download_media(
+        message=message, file_name='root/jetg')
+    try:
+        response = upload_file(download_location)
+    except:
+        await msg.edit_text("Photo size should be less than 5mb!") 
+    else:
+        await msg.edit_text(f'**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @i_ajit**',
+            disable_web_page_preview=True,
+        )
+    finally:
+        os.remove(download_location)
