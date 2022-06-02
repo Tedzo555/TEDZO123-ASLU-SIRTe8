17b9cc3df19f8e749f4b3f78d369a433b052f6ba
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2022-06-02 09:43:48 +0530

    Update filters.py

--- pyrogrambot/filters.py
+++ pyrogrambot/filters.py
@@ -10,8 +10,8 @@ async def kgf_filter(bot, message):
         photo=random.choice(KGF_PHOTOS),
         reply_markup=InlineKeyboardMarkup(KGF_D_BUTTON)
     )
-@Client.on_message(filters.regex("kgf 2") & filters.private)
-async def kgf_filter(bot, message):
+@Client.on_message(filters.regex("Kgf 2") & filters.private)
+async def kgfto_filter(bot, message):
     await message.reply_photo(
         photo=random.choice(KGF_PHOTOS),
         reply_markup=InlineKeyboardMarkup(KGF_D_BUTTON)
