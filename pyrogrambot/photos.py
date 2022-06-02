68920209b4917eada1085bbaeb426e4f62a0244c
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2022-06-02 09:17:05 +0530

    Update photos.py

--- pyrogrambot/photos.py
+++ pyrogrambot/photos.py
@@ -15,3 +15,11 @@ KGF_PHOTOS = [
              "https://telegra.ph/file/1cd858423bfae92d1d193.jpg",
              "https://telegra.ph/file/ecd4e26d057c1c23324fa.jpg"
              ]
+PHOTOS = [
+         "https://telegra.ph/file/1df4930d3ae2aa00bc1cc.jpg",
+         "https://telegra.ph/file/bba3cf572f1a2c1e1743a.jpg",
+         "https://telegra.ph/file/fd1beca91be9d6978445c.jpg",
+         "https://telegra.ph/file/cf681ff662b0677edbcec.jpg",
+         "https://telegra.ph/file/44aa6ae186e239c9716a2.jpg",
+         "https://telegra.ph/file/40021e2256aa82faed22d.jpg"
+         ]
