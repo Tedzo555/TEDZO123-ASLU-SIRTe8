73baf35f4324a05e848e209c3d176cc486daa1a6
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2022-06-02 09:38:31 +0530

    Update filters.py

--- pyrogrambot/filters.py
+++ pyrogrambot/filters.py
@@ -4,6 +4,12 @@ from pyrogrambot.photos import KGF_PHOTOS,PHOTOS
 from pyrogrambot.buttons import KGF_D_BUTTON,TEDZO_BUTTON
 import random
 
+@Client.on_message(filters.regex("kgf2") & filters.private)
+async def kgf_filter(bot, message):
+    await message.reply_photo(
+        photo=random.choice(KGF_PHOTOS),
+        reply_markup=InlineKeyboardMarkup(KGF_D_BUTTON)
+    )
 @Client.on_message(filters.regex("kgf 2") & filters.private)
 async def kgf_filter(bot, message):
     await message.reply_photo(
